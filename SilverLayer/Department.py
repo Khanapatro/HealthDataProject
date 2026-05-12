e3e8eb2b7028db6351863a93c36f184fed03e6d6
Khana Patro <khanapatro84@gmail.com>
2026-05-12 07:50:41 +0530

    Create Department.py

---
+++ SilverLayer/Department.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession, functions as f
+
+#Reading Hospital A departments data 
+df_hosa=spark.read.parquet("/mnt/bronze/hosa/departments")
+
+#Reading Hospital B departments data 
+df_hosb=spark.read.parquet("/mnt/bronze/hosb/departments")
+
+#union two departments dataframes
+df_merged = df_hosa.unionByName(df_hosb)
+
+# Create the dept_id column and rename deptid to src_dept_id
+df_merged = df_merged.withColumn("SRC_Dept_id", f.col("deptid")) \
+                     .withColumn("Dept_id", f.concat(f.col("deptid"),f.lit('-'), f.col("datasource"))) \
+                     .drop("deptid")
+
+df_merged.createOrReplaceTempView("departments")
